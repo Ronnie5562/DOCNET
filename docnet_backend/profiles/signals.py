4fa88398354cd88eae3229b8c041c01167615cb8
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-06-29 14:40:43 +0200

    Created: Added signals to the profiles app

---
+++ docnet_backend/profiles/signals.py
@@ -0,0 +1,16 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from doctor_profiles.models import Doctor
+from patient_profiles.models import Patient
+from django.contrib.auth.models import User
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a user profile when a new user is created
+    """
+    if created and instance.role == 'patient':
+        Patient.objects.create(user=instance)
+    elif created and instance.role == 'doctor':
+        Doctor.objects.create(user=instance)
