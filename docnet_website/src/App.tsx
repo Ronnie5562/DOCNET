d7d7444818f74cca8da8ae527ded061a32098791
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-04 15:00:29 +0200

    Created: Built out the App file

---
+++ docnet_website/src/App.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { PaletteMode } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import AppAppBar from './components/AppAppBar';
+import Hero from './components/Hero.tsx';
+import LogoCollection from './components/LogoCollection';
+import Highlights from './components/Highlights';
+// import Pricing from './components/Pricing';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+import createMuiTheme from './theme/theme.tsx';
+
+
+const App = () => {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const theme = createTheme(createMuiTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+      <Box sx={{ bgcolor: 'background.default' }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        {/* <Pricing /> */}
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
