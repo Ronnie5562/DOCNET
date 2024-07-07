b0b2717b0ba5c5874c5fdf6e7d205e618c7ba8c2
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-07 23:36:05 +0200

    Modified: Added the new pages to the main app router

--- docnet_frontend/src/App.tsx
+++ docnet_frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./pages/auth/LogIn"
+import Register from "./pages/auth/Register"
+import { AuthServiceProvider } from "./context/AuthContext";
+import ConfirmAccount from "./pages/auth/AccountConfirmation";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AuthServiceProvider>
+        <Routes>
+          {/* AUTH ROUTES */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/confirm-account" element={<ConfirmAccount />} />
+        </Routes>
+      </AuthServiceProvider>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
