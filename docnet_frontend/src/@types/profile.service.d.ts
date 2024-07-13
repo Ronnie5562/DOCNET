e88d43f7b535d1b5cb98c95ded39f83e02f7b3a8
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-13 12:56:55 +0200

    Created: Added types to handle user data

---
+++ docnet_frontend/src/@types/profile.service.d.ts
@@ -0,0 +1,66 @@
+export interface ProfileServiceProps {
+    // login: (username: string, password: string) => any;
+    isLoggedIn: boolean;
+    refreshAccessToken: () => Promise<void>
+    getUserAccountDetails: () => any;
+    getUserProfileDetails: () => any;
+    // register: (username: string, password: string, firstname: string, lastname: string) => Promise<any>;
+}
+
+// This type is used to receive data stored in localstorage when user logs in.
+export interface UserDataType {
+    email: string,
+    role: string,
+    first_name: string,
+    last_name: string,
+    picture: string,
+}
+
+
+export interface UserAccountDataType {
+    id: string,
+    timezone: string,
+    email: string,
+    role: string,
+    first_name: string,
+    last_name: string,
+    phone_number: string,
+    picture: string,
+    date_joined: string
+}
+
+export interface DocumentType {
+    id: number;
+    caption: string;
+    file: string;
+    profile: string;
+    uploaded_at: string;
+    modified_at: string;
+};
+
+export interface PatientProfileDataType {
+
+}
+
+export interface DoctorProfileDataType {
+    id: string;
+    user: UserAccountData;
+    bio: string;
+    date_of_birth: string;
+    gender: string;
+    address: string;
+    zip_code: string;
+    city: string;
+    country: string;
+    languages: Record<string, string>;
+    created_at: string;
+    updated_at: string;
+    speciality: string;
+    license_number: string;
+    years_of_experience: number;
+    education: string;
+    certifications: string;
+    awards: string;
+    professional_statement: string;
+    documents: Document[];
+}
