0139cb59dcd22a477dfe83a90f83eac70240ce46
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-20 09:42:54 +0200

    Modified: Added new packages to the project.

--- docnet_frontend/vite.config.ts
+++ docnet_frontend/vite.config.ts
@@ -1,7 +1,28 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//       "date-fns": path.resolve(__dirname, "node_modules/date-fns/esm"),
+//     },
+//   },
+//   optimizeDeps: {
+//     include: [
+//       'date-fns',
+//       'date-fns/_lib/format/longFormatters',
+//     ],
+//   },
+// })
