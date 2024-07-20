79e0092c8b126a328af77efb941784fbc55bfdd7
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-20 09:31:11 +0200

    Created: Built out the appointments APIs

--- docnet_backend/appointments/views.py
+++ docnet_backend/appointments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render # noqa
+from appointments.models import Appointment
+from rest_framework import generics, permissions
+from utils.permissions import IsUserInvolvedOrAdmin
+from appointments.serializers import AppointmentSerializer
 
-# Create your views here.
+
+class AppointmentsListCreateView(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AppointmentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsUserInvolvedOrAdmin]
