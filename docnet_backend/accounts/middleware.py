1e3a707f7a59aa6620c2ef9d23b924b8a8c93060
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-07-30 22:42:17 +0200

    Modified: Fixed issues with csrf tokens

--- docnet_backend/accounts/middleware.py
+++ docnet_backend/accounts/middleware.py
@@ -6,6 +6,11 @@ from django.utils.deprecation import MiddlewareMixin
 class JWTAuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         token_name = settings.SIMPLE_JWT["ACCESS_TOKEN_NAME"]
+
         if token_name in request.COOKIES:
             token = request.COOKIES[token_name]
+            csrf_token = request.COOKIES.get("csrftoken", "")
+
             request.META['HTTP_AUTHORIZATION'] = f'Bearer {token}'
+            request.META['HTTP_X_CSRFTOKEN'] = csrf_token
+
