f284802daee89caf07cbd953658d98f247d6e10c
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-08-02 18:04:16 +0200

    Deployment

--- docnet_backend/docnet_backend/settings.py
+++ docnet_backend/docnet_backend/settings.py
@@ -57,14 +57,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "accounts.middleware.JWTAuthenticationMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "accounts.middleware.JWTAuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "docnet_backend.urls"
