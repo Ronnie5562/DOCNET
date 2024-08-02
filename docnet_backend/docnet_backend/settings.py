57eed59990f617cbcd23fb3eb1e12e3b37e0616a
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-08-02 11:59:01 +0200

    Deployment

--- docnet_backend/docnet_backend/settings.py
+++ docnet_backend/docnet_backend/settings.py
@@ -27,6 +27,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://docnet-frontend.onrender.com',
+    'https://docnet-test.onrender.com',
 ]
 
 # Application definition
