f5740f93e5bde8c99fdbf5962aaf46e57e9a5257
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-08-02 15:34:31 +0200

    Fixed issues with CORS configuration in production

--- docnet_frontend/src/config.ts
+++ docnet_frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000/api";
-// export const BASE_URL = "https://docnet-backend.onrender.com/api";
+// export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://docnet-backend.onrender.com/api";
 export const MEDIA_URL = "https://docnet-backend.onrender.com/";
 export const WS_ROOT = "ws://docnet-backend.onrender.com/"
