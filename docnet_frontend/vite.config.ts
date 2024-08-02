f2e626232a171a6d5fdda42b7cc1cbdf7fb565fd
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-08-02 11:08:37 +0200

    Deployment

--- docnet_frontend/vite.config.ts
+++ docnet_frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://docnet-frontend.onrender.com/',
+  // base: 'https://docnet-frontend.onrender.com/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
